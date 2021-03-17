0a535ad5d203eccbf8f339f89affb1c4d80a2879
carlalopxz <carlalopezprogamacion@gmail.com>
2021-03-17 20:51:07 -0300

    Agregue un ejemplo

--- Ejemplos.py
+++ Ejemplos.py
@@ -1,6 +1,17 @@
 #Acá voy a poner ejemplos que encuentre
 
-#Ejemplo 1
+#Ejemplo del diagrama de flujo 
+
+totalAlumnos = input('Ingrese el número total de alumnos')
+numeroDeMujeres = input('Ingrese el número de mujeres que hay en el salon')
+
+def porcentajeAlumnas(totalAlumnos, numeroDeMujeres):
+    resultado1 = int((numeroDeMujeres*100)/totalAlumnos)
+    return resultado1
+    print(resultado1)
+
+porcentajeAlumnas(totalAlumnos, numeroDeMujeres)
+
 #Funciones
 
 def mensaje():
@@ -21,3 +32,13 @@ def suma2(num1, num2):
 suma2(3,4)
 suma2(5,8)
 
+def suma3(num3,num4):
+    resultado = num3 + num4
+    return resultado
+
+print(suma3(3,4))
+print(suma3(5,8))
+
+almacena_resultado = suma3(6,5)
+
+print(almacena_resultado)
