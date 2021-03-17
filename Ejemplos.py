07435f60336d262e846c640b1267d33b6406a162
carlalopxz <carlalopezprogamacion@gmail.com>
2021-03-17 18:06:58 -0300

    Agregue funciones

--- Ejemplos.py
+++ Ejemplos.py
@@ -7,3 +7,17 @@ def mensaje():
     print('Estamos aprendiendo funciones')
 
 mensaje()
+
+def suma1():
+    num1 = 5
+    num2 = 7
+    print(num1+num2)
+
+suma1()
+
+def suma2(num1, num2):
+    print(num1 + num2)
+
+suma2(3,4)
+suma2(5,8)
+
