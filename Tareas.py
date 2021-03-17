b8fc9f01536cb9f46044edbe9b821bc4f7544770
carlalopxz <77545114+carlalopxz@users.noreply.github.com>
2021-03-17 15:15:46 -0300

    Add files via upload

---
+++ Tareas.py
@@ -0,0 +1,17 @@
+#Tarea1
+
+print('Hola mundo!')
+print('Por favor, ingresa tu nombre')
+nombre = input()
+print('Bienvenido ' + nombre)
+
+#Tarea2
+#Hacer un programa que te pregunte tu edad y te diga cuantos vas a cumplir este año
+
+edad = input('¿Cuantos años tenes?')
+def edadAcumplir(numero1,numero2):
+    return numero1 + numero2
+    
+print("vas a cumplir " + str(edadAcumplir(int(edad),1)))
+
+#Tarea3
