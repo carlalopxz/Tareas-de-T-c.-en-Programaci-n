2c24a66886310e7db05244110d24ab924ee3431f
carlalopxz <carlalopezprogamacion@gmail.com>
2021-03-26 21:59:11 -0300

    agregue ejericicio

--- Tareas del curso/09_multiplos.py
+++ Tareas del curso/09_multiplos.py
@@ -1,1 +1,13 @@
 #Pedir dos números y decir si uno es múltiplo del otro.
+
+x = 0
+y = 0 
+
+print('Digite dos números')
+x = int(input())
+y = int(input())
+
+if x % y == 0:
+    print('El numero ' + str(x) + ' es multiplo de ' + str(y))
+else:
+    print('El numero ' + str(x) + ' no es multiplo de ' + str(y))
