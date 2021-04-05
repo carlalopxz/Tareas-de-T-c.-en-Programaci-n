14fcb1863563e8f9a3d1404fa3e9a760fd9df504
carlalopxz <carlalopezprogamacion@gmail.com>
2021-04-05 12:53:51 -0300

    Agregue ejercicios

---
+++ Tareas del curso/14_notas_2.py
@@ -0,0 +1,29 @@
+#Ejercicio11.Pedir una nota numérica entera entre 0 y 10, y mostrar dicha nota de la forma: 
+#cero, uno, dos, tres, cuatro, cinco, seis, siete, ocho, nueve, diez.
+
+num = int(input('Digite un numero del 0 al 10 \n'))
+
+if num < 0 and num > 10:
+    print('Numero invalido')
+elif num == 0:
+    print('CERO')
+elif num == 1:
+    print('UNO')
+elif num == 2:
+    print('DOS')
+elif num == 3:
+    print('TRES')
+elif num == 4:
+    print('CUATRO')
+elif num == 5:
+    print('CINCO')
+elif num == 6:
+    print('SEIS')
+elif num == 7:
+    print('SIETE')
+elif num == 8:
+    print('OCHO')
+elif num == 9:
+    print('NUEVE')
+else:
+    print('DIEZ')
