96b3d2cbdd34d5da60203bbef37603a97933f6b9
carlalopxz <carlalopezprogamacion@gmail.com>
2021-04-07 00:10:26 -0300

    Cambios en ejercicios

--- Tareas del curso/16_.py
+++
@@ -1,9 +0,0 @@
-#Tarea: Ejercicio 13 Pedir el día, mes y año de una fecha e indicar si la fecha es correcta. Suponiendo
-#todos los meses de 30 días.
-'''
-dia = input('Ingrese el dia')
-mes = input('Ingrese el mes')
-año = input('ingrese el año')
-
-print(dia,mes,año)
-'''
