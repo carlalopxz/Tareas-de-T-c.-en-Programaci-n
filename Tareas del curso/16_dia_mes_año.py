402d79b86c97035910406364ccfaf0dfdbeaede1
carlalopxz <carlalopezprogamacion@gmail.com>
2021-04-07 11:31:13 -0300

    cambios en ejercicio 16

--- Tareas del curso/16_dia_mes_año.py
+++ Tareas del curso/16_dia_mes_año.py
@@ -1,21 +1,12 @@
 #Pedir el día, mes y año de una fecha e indicar si la fecha es correcta. 
 #Suponiendo todos los meses de 30 días.
-'''
-dia = input('Ingrese el dia')
-mes = input('Ingrese el mes')
-año = input('ingrese el año')
-
-print(dia,mes,año)
-'''
 
 dia = int(input('Ingrese un dia \n'))
 mes = int(input('Ingrese un mes \n'))
 año = int(input('Ingrese un año \n'))
 
-if dia <= 0 or dia >= 31:
-    print('El dia es invalido')
-elif mes <= 0 or mes >= 13:
-    print('El mes es invalido')
+if dia <= 0 or dia >= 31 or mes <= 0 or mes >= 13 or año < 0:
+    print('El dato es invalido')
 else:
     1 >= dia <= 30
     1 >= mes <= 12
