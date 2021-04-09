1c393865ed54528005320f7a5845a72270bcc179
carlalopxz <carlalopezprogamacion@gmail.com>
2021-04-09 16:12:49 -0300

    Ejercicios nuevos

---
+++ Tareas del curso/Variables y Condicionales/20_.py
@@ -0,0 +1,4 @@
+#Ejercicio 15. Pedir dos fechas y mostrar el número de días que hay de diferencia. 
+# Suponiendo todos los meses de 30 días.
+
+#Ejercicio17.Ídem que el ej. 15, con meses de 28, 30 y 31 días. Sin años bisiestos.
