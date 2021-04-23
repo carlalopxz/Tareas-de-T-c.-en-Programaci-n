942a911dbdab0ba59a2dbed250f95319dada75c4
carlalopxz <carlalopezprogamacion@gmail.com>
2021-04-23 12:37:16 -0300

    nuevo

---
+++ Bucles y ciclos de repeticion/15_edades_alturas.py
@@ -0,0 +1,35 @@
+#Dadas las edades y alturas de 5 alumnos, 
+# mostrar la edad y la estatura media, 
+# la cantidad de alumnos mayores de 18 años, 
+# y la cantidad de alumnos que miden más de 1.75m. (175cm)
+
+edadMedia = 0
+contEdad = 0
+
+alturaMedia = 0
+contAltura = 0
+
+mayoresA18 = 0
+mayoresDe175 = 0
+
+for i in range(5):
+    edades = int(input('Ingrese la edad \n'))
+    alturas = float(input('Ingrese las estaturas \n'))
+    
+    edadMedia = edadMedia + edades
+    contEdad = contEdad + 1
+    
+    alturaMedia = alturaMedia + alturas
+    contAltura = contAltura + 1
+
+    if edades > 18:
+        mayoresA18 = mayoresA18 + 1
+
+    if alturas > 1.75:
+        mayoresDe175 = mayoresDe175 + 1
+
+print('La edad media es de: ' + str(edadMedia/contEdad))
+print('La estatura media es de: ' + str(alturaMedia/contAltura))
+
+print('La cantidad mayores a 18 son: ' + str(mayoresA18))
+print('La cantidad mayores a 1.75 son: ' + str(mayoresDe175))
