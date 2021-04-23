499c6ced049eae3777edfffb0a51ad061de40d80
carlalopxz <carlalopezprogamacion@gmail.com>
2021-04-23 12:35:38 -0300

    Nuevo nombre

--- Bucles y ciclos de repeticion/13_positivos_negativos_ceros.py
+++ Bucles y ciclos de repeticion/13_positivos_negativos_ceros.py
@@ -4,17 +4,3 @@
 # y la cantidad de ceros ingresados. 
 
 #Promedio = Suma / Cantidad
-
-num = ''
-cont = 0 
-sumaPositivos = 0
-for i in range(1,11):
-    num = int(input('Ingrese un numero \n'))
-
-    while True:
-        if num > 0:
-            break
-        sumaPositivos = sumaPositivos + num
-        cont = cont + 1
-        print('Suma de los numeros: '+str(sumaPositivos))
-        print('Promedio de los numeros: '+str(int(sumaPositivos/cont)))
