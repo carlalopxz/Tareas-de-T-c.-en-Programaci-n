52048807ec5b8923bc10ea5a6b746f951d9cf24d
carlalopxz <carlalopezprogamacion@gmail.com>
2021-05-09 18:16:15 -0300

    Resueltos

---
+++ Listas/02_lista2.py
@@ -0,0 +1,8 @@
+#Leer 5 números, guardarlos en una lista y mostrarlos en orden inverso al introducido.
+
+lista_num = []
+
+for i in range(5):
+    lista_num.append(int(input('Ingrese un número \n')))
+
+    print()
