55addff888d2477296ef346a39a4dda17c605ae0
carlalopxz <carlalopezprogamacion@gmail.com>
2021-05-25 23:27:11 -0300

    trabajo practico

--- ajedrez.py
+++ ajedrez.py
@@ -11,16 +11,35 @@ import random
 tablero = ['__'] * 8
 for i in range(8):
     tablero[i] = ['__'] * 8
-# Posicionamiento de piezas 
-piezas = ['pB','tB','cB','aB','rB','dB','pN','tN','cN','aN','rN','dN']
-for blancas in piezas:
-    pB = random.randint(0,8)
-    tB = random.randint(0,2)
-    cB = random.randint(0,2)
-    aB = random.randint(0,2)
-    rB = random.randint(0,1)
-    dB = random.randint(0,1)
-    
+# Posicionamiento de piezas blancas
+pB = random.randint(0,8)
+tB = random.randint(0,2)
+cB = random.randint(0,2)
+aB = random.randint(0,2)
+rB = 1
+dB = random.randint(0,1)
+piezasBlancas = [pB,tB,cB,aB,rB,dB]
+pBlanc = ['pB','tB','cB','aB','rB','dB']
+indice_blancas=0
+for i in piezasBlancas:
+    for j in range(i):
+        tablero[random.randint(0,7)][random.randint(0,7)] = pBlanc[indice_blancas]
+    indice_blancas += 1
+#Posicionamiento de piezas negras 
+pN = random.randint(0,8)
+tN = random.randint(0,2)
+cN = random.randint(0,2)
+aN = random.randint(0,2)
+rN = 1
+dN = random.randint(0,1)
+piezasNegras = [pN,tN,cN,aN,rN,dN]
+pBlanc = ['pN','tN','cN','aN','rN','dN']
+indice_negras=0
+for i in piezasBlancas:
+    for j in range(i):
+        tablero[random.randint(0,7)][random.randint(0,7)] = pBlanc[indice_negras]
+    indice_negras += 1
+#Impresiòn de tablero
 for x in tablero:
     print(x)     
 
